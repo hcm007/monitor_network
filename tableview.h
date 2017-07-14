42106ef3508a2cdee49ecae5cae2048e234b1dab
hcm007 <hcm007c@gmail.com>
2017-07-14 08:46:40 -0400

    Update tableview.h

--- tableview.h
+++ tableview.h
@@ -1,38 +1,23 @@
 #ifndef TABLEVIEW_H
 #define TABLEVIEW_H
-
 #include <QMainWindow>
 #include <QProcess>
-
 namespace Ui {//this is created by the ui interface
 class TableView;
 }
-
 class TableView : public QMainWindow
 {
     Q_OBJECT
-
 public:
     explicit TableView(QWidget *parent = 0);
-
     ~TableView();
-
-
-
 private:
     Ui::TableView *ui;
-
 public:
     void initializetablewidget();
-
-
 private slots:
     void touchOkButton();
-
-void changejudge();
-
-
-
+    void changejudge();
 
 };
 
