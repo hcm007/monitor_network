93d377addefcacbe5adc58893f38c3d2dc5f8970
hcm007 <hcm007c@gmail.com>
2017-07-14 08:30:33 -0400

    Update hashmap.h

--- hashmap.h
+++ hashmap.h
@@ -15,39 +15,31 @@ int previoushashmaplength=0;//++++++++++++++++++++++++
 #define TRUE 1
 #define FALSE 0
 const char thecomputer[length] = "00:21:70:5f:b3:62";
-
 struct node_1
 {
-
-	char ipaddress[length];//just restore the ip address souce ip or destination ip
-	int count;//restore the number of bytes of paload
-	int portnumber;//restore the port number of ip
-	int timenumber;//restore the how many time of ip address
-
-	char application[length];//to restore the information
-	char transitionprotocal[length];
-
-	struct node_1* next;
+    char ipaddress[length];//just restore the ip address souce ip or destination ip
+    int count;//restore the number of bytes of paload
+    int portnumber;//restore the port number of ip
+    int timenumber;//restore the how many time of ip address
+    char application[length];//to restore the information
+    char transitionprotocal[length];
+    struct node_1* next;
 };
-
 typedef struct node_1 node;
-
 node* initialize()
 {
-	node* a = (node*)malloc(sizeof(node));
-	strcpy(a->ipaddress, "");
-	//+++++++++++++++++++
-	strcpy(a->application, "");// to initialize the application
-	strcpy(a->transitionprotocal, "");
-	//+++++++++++++++++++++++++++++
-
-	a->count = 0;
-	a->portnumber = 0;
-	a->timenumber = 1;
-	a->next = NULL;
-	//printf("initialize is called %x\n",a);
-
-	return a;
+    node* a = (node*)malloc(sizeof(node));
+    strcpy(a->ipaddress, "");
+//+++++++++++++++++++
+    strcpy(a->application, "");// to initialize the application
+    strcpy(a->transitionprotocal, "");
+//+++++++++++++++++++++++++++++
+    a->count = 0;
+    a->portnumber = 0;
+    a->timenumber = 1;
+    a->next = NULL;
+//printf("initialize is called %x\n",a);
+    return a;
 };
 
 //++++++++++++++++++++++insert the node in the link+++++++++++++++++++++++++++++
@@ -64,21 +56,18 @@ last=newone;
 //search every link if there is one node exist return the positin otherwise return 0
 int search(node* first, node* newone)
 {
-
-	// printf("search is called\n");
-	int position = 0;
-	node* head = first;
-	int pose = 0;
-	while (head->next != NULL) {
-
-		position++;
-		if ((strcmp(head->ipaddress, newone->ipaddress) == 0) && ((head->portnumber) == (newone->portnumber)))
-		{
-			pose = position;
-			break;
-		}
-		head = head->next;
-
+// printf("search is called\n");
+    int position = 0;
+    node* head = first;
+    int pose = 0;
+    while (head->next != NULL) {
+	position++;
+	if ((strcmp(head->ipaddress, newone->ipaddress) == 0) && ((head->portnumber) == (newone->portnumber)))
+	{
+	    pose = position;
+            break;
+	}
+	    head = head->next;
 	}
 	//printf("position in search %d\n",pose);
 	return pose;
@@ -87,15 +76,14 @@ int search(node* first, node* newone)
 //add the exist number in the node
 void additioncount(node* first, int position, node* newone)
 {
-
-	//printf("addition is called\n");
-	node* head = first; 
-	for (int i = 0; i<position - 1; i++) {
-		head = head->next;
+//printf("addition is called\n");
+    node* head = first; 
+    for (int i = 0; i<position - 1; i++) {
+	head = head->next;
 	}
-	head->count = head->count + newone->count;
-	head->timenumber = head->timenumber + newone->timenumber;
-	free(newone);//you should be careful zheli
+    head->count = head->count + newone->count;
+    head->timenumber = head->timenumber + newone->timenumber;
+    free(newone);//you should be careful zheli
 
 };
 
@@ -122,11 +110,6 @@ void showmap(node* first)
 };
 
 */
-
-
-
-
-
 
 
 
