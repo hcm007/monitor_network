99ee2ce388a711771c372d5eea3a45320c592f34
hcm007 <hcm007c@gmail.com>
2017-07-14 08:24:45 -0400

    Update comeinhashmap.h

--- comeinhashmap.h
+++ comeinhashmap.h
@@ -7,43 +7,30 @@
 //
 
 #include "port.h"
-
-
-
-
 node* comeinfirstnode;
 node* comeinlastnode;
 int comeinnumber = 1;
-
 void comeinsert(node* newone)
 {
 	//printf("insert is called\n");
-	comeinlastnode->next = newone;
-	comeinlastnode = newone;
+    comeinlastnode->next = newone;
+    comeinlastnode = newone;
 };
-
-
-
-
-
-
 void comeinhashmapfunction(char* ip, int paylaod, int port)
-
 {
-	// printf("comein is called \n");
-
-	node* newone_1 = initialize();
-	strcpy(newone_1->ipaddress, ip);
-	newone_1->count = paylaod;
-	newone_1->portnumber = port;
-	int position_1 = search(comeinfirstnode, newone_1);
-	if (position_1>0) {
-		additioncount(comeinfirstnode, position_1, newone_1);
-	}
-	if (position_1 == 0) {
-		comeinsert(newone_1);
-		comeinnumber++;
-	}
+// printf("comein is called \n");
+    node* newone_1 = initialize();
+    strcpy(newone_1->ipaddress, ip);
+    newone_1->count = paylaod;
+    newone_1->portnumber = port;
+    int position_1 = search(comeinfirstnode, newone_1);
+    if (position_1>0) {
+	additioncount(comeinfirstnode, position_1, newone_1);
+    }
+    if (position_1 == 0) {
+	comeinsert(newone_1);
+	comeinnumber++;
+    }
 
 };
 
