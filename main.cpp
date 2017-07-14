60ca9c443f5fd79703a171835b64e292fd21b0d3
hcm007 <hcm007c@gmail.com>
2017-07-14 08:31:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,53 +3,12 @@
 #include <QTextCodec>
 //#include "hashmapmainfunction.h"
 #include "tableview.h"
-
-
-
-
-
 int main(int argc, char *argv[])
 {
-
-
-
-
     QApplication a(argc, argv);
     TableView w;
     w.initializetablewidget();
     w.show();
-
 //+++++++++++++++++++++this is  main from the hash function
-
-
-
-
-
-
     return a.exec();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
